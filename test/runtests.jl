6fa7b1cd67d68e4e994deea67bb721f31978f1d7
GabrielBrown <43052327+Amphibianoid@users.noreply.github.com>
2018-11-20 14:31:44 -0800

    Update runtests.jl
    Adding trial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "NewtonsMethod.jl" begin
     # Write your own tests here.
+    @test foo() == 0.11388071406436832
+@test foo(1, 1.5) == 0.2731856314283442
+@test_broken foo(1, 0) # tells us this is broken
 end
