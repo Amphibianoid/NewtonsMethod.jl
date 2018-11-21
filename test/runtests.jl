974ef0a129149e378642b4c96ad0399964995768
Gabriel Brown <43052327+Amphibianoid@users.noreply.github.com>
2018-11-20 18:58:39 -0800

    Update runtests.jl
    Removing Random stuff

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,6 @@
 using NewtonsMethod
 using Test
-using LinearAlgebra
-
-#@testset "NewtonsMethod.jl" begin
-    # Write your own tests here.
-#@test foo() == 0.11388071406436832
-#@test foo(1, 1.5) == 0.2731856314283442
-#@test_broken foo(1, 0) # tells us this is broken
+#using LinearAlgebra
 
 # Testing first function
 
